01ff1062a03844632f516545577a2135ce794f19
El Alklde <alklde@MacBook-Pro.local>
2021-12-02 12:03:49 +0100

    NN

--- src/views/pages/createView.php
+++ src/views/pages/createView.php
@@ -1,30 +1,33 @@
 <?php require_once __DIR__ . "/../layouts/headbydefault.php" ?>
 
-<form  method="POST" action="checklist" 
-class='w-75 pt-5 pb-5 "d-flex justify-content-center align-items-center' 
-style="background-color:#6B93C6;height:77vh;">
-    <div class="d-flex justify-content-center align-items-center flex-column">
+<div class="w-100" style="display:flex; justify-content:center; align-items:center;"> 
+    <form  method="POST" action="checklist" 
+    class='min-vw-75 pt-5 pb-5 "d-flex justify-content-center align-items-center' 
+    style="background-color:#6B93C6; height:77vh; ">
+        <div class="d-flex justify-content-center align-items-center flex-column">
 
-        <div class="mb-3 w-75">
-            <input id="inputName" type="text" class="form-control" required name="name" placeholder="Name/Surname">
+            <div class="mb-3 w-75">
+                <input id="inputName" type="text" class="form-control" required name="name" placeholder="Name/Surname">
+            </div>
+
+            <div class="mb-3 w-75">
+                <input id="inputEmail" type="email" class="form-control" required name="email" placeholder="name@example.com">
+            </div>
+
+            <div class="mb-3 w-75">
+                <input id="inputQueryTitle" type="title" class="form-control" required name="title_query" aria-describedby="emailHelpId"
+                placeholder="title">
+            </div>
+
+            <div class="mb-3 w-75">
+                <textarea class="form-control" required name="problem_query" rows="3"  placeholder="Message..."></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+            
         </div>
+    </form>
+</div>
 
-        <div class="mb-3 w-75">
-            <input id="inputEmail" type="email" class="form-control" required name="email" placeholder="name@example.com">
-        </div>
-
-        <div class="mb-3 w-75">
-            <input id="inputQueryTitle" type="title" class="form-control" required name="title_query" aria-describedby="emailHelpId"
-            placeholder="title">
-        </div>
-
-        <div class="mb-3 w-75">
-            <textarea class="form-control" required name="problem_query" rows="3"  placeholder="Message..."></textarea>
-        </div>
-
-        <button type="submit" class="btn btn-primary">Submit</button>
-        
-    </div>
-</form>
 
 <?php require_once __DIR__ . "/../layouts/defaultFooter.php" ?>
