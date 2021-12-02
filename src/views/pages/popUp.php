46959d55c681444b6ac63a1e5fe441db33aa6b47
timfel20 <timmyfelicia@gmail.com>
2021-12-02 12:23:06 +0100

    popup styles

--- src/views/pages/popUp.php
+++ src/views/pages/popUp.php
@@ -1,9 +1,9 @@
-<?php require_once __DIR__ . '/../layouts/default.php'?>
-<?php 
+<?php require_once __DIR__ . '/../layouts/default.php' ?>
+<?php
 echo "
     <div class='container d-flex justify-content-center mt-3' style='height:80%;'>
         <div id='POPup' class=' d-flex justify-content-center ' >
-            <div style='display:flex; flex-direction:column; align-items:center; border-radius:15px;background-color:#3954A5'>
+            <div style='display:flex; flex-direction:column; margin-bottom: 17px; align-items:center; border-radius:15px;background-color:#3954A5; min-width: 170%'>
 
                 <div style='width: 90%;'>
                     <header class='d-flex justify-content-between'> 
@@ -17,7 +17,7 @@ echo "
                         <p class=' mt-4 font-weight-bold ' style='font-size:1em;'><b>{$dataConsulta['problem_query']}</b></p>
                     </main>
             
-                    <footer class='d-flex justify-content-center mt-3 mb-4 ml-4 position-relative'>
+                    <footer class='d-flex justify-content-center mt-3 ml-4 position-relative'>
                         <button type='button' onclick='location=`/`' class='btn p-0 position-absolute start-0'>
                             <img src='/assets/btnback.png' class='img-fluid' style='width:70%;' alt='btn-back'>
                         </button>
@@ -33,4 +33,4 @@ echo "
 ";
 ?>
 
-<?php require_once __DIR__ . '/../layouts/defaultFooter.php'?>
+<?php require_once __DIR__ . '/../layouts/defaultFooter.php' ?>
