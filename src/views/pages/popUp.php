87d6278f81a3c2f3f486dcdc2324e15bad217176
El Alklde <alklde@MacBook-Pro.local>
2021-12-03 08:53:27 +0100

    correction

--- src/views/pages/popUp.php
+++ src/views/pages/popUp.php
@@ -11,13 +11,13 @@ echo "
                         <p class='text-white mr-1 mt-2'>{$dataConsulta['created_at']}</p>
                     </header>
                 
-                    <main class='rounded p-5 h-75 w-100' style='background-color:#ABC6E6;'>
+                    <main class='rounded px-5 pt-5 pb-4 w-100' style='background-color:#ABC6E6;'>
                         <p class=' mb-3 font-weight-bold ' style='font-size:1em;'><b>{$dataConsulta['name']}</b> </p>
                         <p class=' mb-5 font-weight-bold ' style='font-size:1em;'><b>{$dataConsulta['email']}</b></p>
                         <p class=' mt-4 font-weight-bold ' style='font-size:1em;'><b>{$dataConsulta['problem_query']}</b></p>
                     </main>
             
-                    <footer class='d-flex justify-content-center mt-3 ml-4 position-relative'>
+                    <footer class='d-flex justify-content-center mt-4 ml-4 position-relative'>
                         <button type='button' onclick='location=`/`' class='btn p-0 position-absolute start-0'>
                             <img src='/assets/btnback.png' class='img-fluid' style='width:70%;' alt='btn-back'>
                         </button>
